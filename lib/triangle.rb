75c3d651a8198850c7fc3afc26c6b9310c738610
Matt Eva <matteweva@gmail.com>
2021-11-26 13:42:09 -0800

    commit tree 5cd3da3d1d686b07153ad6a69d3ea34ec286ad8c

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,33 @@
 class Triangle
   # write code here
+  def initialize (side1, side2, side3)
+    @side1 = side1
+    @side2 = side2
+    @side3 = side3
+    @sides = [@side1, @side2, @side3]
+  end
+
+  def kind
+    if (@side1 >= @side2 + @side3 || @side2 >= @side3 + @side1) || @side3 >= @side1 + @side2
+      raise TriangleError
+    elsif (@side1 == 0 || @side2 == 0) || @side3 ==0
+      raise TriangleError
+    else
+      if @side1 == @side2 && @side2 == @side3
+        :equilateral
+      elsif @side1 == @side2 || @side1 == @side3 
+        :isosceles
+      elsif @side2 == @side3
+        :isosceles
+      else
+        :scalene
+      end
+    end
+
+  end
+
+  class TriangleError < StandardError
+      puts "Not a triangle: no sides can have a length of 0, and the sum of two sides must always exceed the length of the third side."
+  end
+
 end
